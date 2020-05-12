1df530c16d827c1b126b541c9a38bda423556a39
kiarash zandi <kiarash.gh.zandi@gmail.com>
2020-05-12 10:51:53 +0430

    tamrin jadvale zarb .

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>PHP</title>
+</head>
+<body>
+    <?php
+    echo "<table>";
+        for ( $x = 1; $x < 10; $x++){
+            echo "<tr>";
+            for ( $y = 1; $y <10; $y++){
+                echo "<td>".$x * $y."</td>";
+            }
+            echo "</tr>";
+        }
+    echo "</table>";
+    ?>
+    <?php
+//    $color = 'RED';
+//    echo 'My car is '. $color . '<br>';
+//    echo 'My hous is '. $color . '<br>';
+//    ?>
+</body>
+</html>
